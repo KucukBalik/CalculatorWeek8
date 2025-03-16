6f427b81e115fa5ad89422ad3f1d1dd7a319035d
Emre Demir <g00427391@atu.ie>
2025-03-16 12:04:28 +0000

    class added

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,6 @@
+public class Calculator {
+    public int add(int first, int second) {
+
+        return first + second;
+    }
+}
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+    Calculator myCalc;
+
+    @BeforeEach
+    void Setup()
+    {
+        myCalc = new Calculator();
+    }
+    @Test
+    void testAddSuccess()
+    {
+        assertEquals(8, myCalc.add(4,4));
+    }
+
+    void testAddFailure()
+    {
+        assertTh
+    }
 }
